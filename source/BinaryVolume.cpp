b86f417b26a797cf83d6c9858e09859faceb3f92
Vascular User 1 <vascularuser1@Users-MacBook-Pro.local>
2017-07-03 13:26:40 -0700

    test

--- source/BinaryVolume.cpp
+++ source/BinaryVolume.cpp
@@ -13,36 +13,46 @@ bool BinaryVolume::is(unsigned int x, unsigned int y, unsigned int z) const {
 		return false;
 	return bits[indexOf(x, y, z)];
 }
+
 bool BinaryVolume::is(unsigned int i) const {
 	return is(x(i), y(i), z(i));
 }
+
 void BinaryVolume::set(unsigned int x, unsigned int y, unsigned int z, bool tf){
 	if(x < size[0] && y < size[1] && z < size[2])
 		bits[indexOf(x, y, z)] = tf;
 }
+
 void BinaryVolume::t(unsigned int x, unsigned int y, unsigned int z){
 	set(x, y, z, true);
 }
+
 void BinaryVolume::t(unsigned int i){
     t(x(i), y(i), z(i));
 }
+
 void BinaryVolume::t(const vector<unsigned int> &v){
 	for(unsigned int i(0); i < v.size(); i++)
 		t(x(v[i]), y(v[i]), z(v[i]));
 }
+
 void BinaryVolume::f(unsigned int x, unsigned int y, unsigned int z){
 	set(x, y, z, false);
 }
+
 void BinaryVolume::f(unsigned int i){
 	f(x(i), y(i), z(i));
 }
+
 void BinaryVolume::f(const vector<unsigned int> &v){
 	for(unsigned int i(0); i < v.size(); i++)
 		f(x(v[i]), y(v[i]), z(v[i]));
 }
+
 void BinaryVolume::clear(){
 	bits = vector<bool>(totalSize(), false);
 }
+
 unsigned int BinaryVolume::findFirstAtOrAfter(unsigned int i) const {
 	unsigned int iMax(totalSize());
 	while(i < iMax){
@@ -52,6 +62,7 @@ unsigned int BinaryVolume::findFirstAtOrAfter(unsigned int i) const {
 	}
 	return i;
 }
+
 unsigned int BinaryVolume::findFirstFalseAtOrAfter(unsigned int i) const {
 	unsigned int iMax(totalSize());
 	while(i < iMax){
@@ -61,9 +72,11 @@ unsigned int BinaryVolume::findFirstFalseAtOrAfter(unsigned int i) const {
 	}
 	return i;
 }
+
 unsigned int BinaryVolume::totalSize() const {
 	return size[0]*size[1]*size[2];
 }
+
 unsigned int BinaryVolume::totalTrue() const {
 	unsigned int totSize(totalSize()), totTrue(0);
 	for(unsigned int i(0); i < totSize; i++){
@@ -72,18 +85,23 @@ unsigned int BinaryVolume::totalTrue() const {
 	}
 	return totTrue;
 }
+
 unsigned int BinaryVolume::indexOf(unsigned int x, unsigned int y, unsigned int z) const {
 	return x*size[1]*size[2] + y*size[2] + z;
 }
+
 unsigned int BinaryVolume::x(unsigned int i) const {
 	return i/(size[1]*size[2]);
 }
+
 unsigned int BinaryVolume::y(unsigned int i) const {
 	return (i%(size[1]*size[2]))/size[2];
 }
+
 unsigned int BinaryVolume::z(unsigned int i) const {
 	return i%size[2];
 }
+
 string BinaryVolume::makeStringPos(unsigned int i) const {
 	stringstream strstr;
 	strstr << "(" << x(i) << ", " << y(i) << ", " << z(i) << ")";
